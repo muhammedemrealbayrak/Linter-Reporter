d07a717aa92a94d916799ef6ce23d8ff1a915e1e
muhammedemrealbayrak <emrealbayrak3162@icloud.com>
2026-05-01 15:00:31 +0300

    İlk versiyon

---
+++ src/main/java/AnalizDosyasi.java
@@ -0,0 +1,15 @@
+public class AnalizDosyasi {
+    public void testMetodu() {
+        int kullanılmayanDegisken = 100; // HATA 1: Tanımlandı ama kullanılmadı
+
+        if (true) {
+            // HATA 2: Boş if bloğu. Gereksiz yer kaplıyor.
+        }
+
+        if (false){
+        }
+        int words=a;
+
+        System.out.println("Sistem çalışıyor...");
+    }
+}
